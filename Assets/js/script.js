b5e0275c142da64a7385495c71a86fe866cbafb7
mustafemohamoud <mustyymo@outlook.com>
2023-10-26 18:31:47 +0100

    start variable created and first questions

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+const startButton = $("#startButton")
+
+function startQuiz() {
+currentQuestionIndex = 0;
+score = 0;
+startButton.innerHTML = "start";
+showQuestion();
+}
+
+function showQuestion() {
+
+}
