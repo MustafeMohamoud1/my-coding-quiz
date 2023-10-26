bacb67bb1857bbb1c139e5ca60d22f3c3285da84
mustafemohamoud <mustyymo@outlook.com>
2023-10-26 23:44:14 +0100

    css styling for question boxes done

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,3 +10,53 @@ showQuestion();
 function showQuestion() {
 
 }
+
+const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "script", correct: true},
+            {text: "scripting", correct: false},
+            {text: "javascript", correct: true},
+            {text: "js", correct: true},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "script", correct: true},
+            {text: "scripting", correct: false},
+            {text: "javascript", correct: true},
+            {text: "js", correct: true},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "script", correct: true},
+            {text: "scripting", correct: false},
+            {text: "javascript", correct: true},
+            {text: "js", correct: true},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "script", correct: true},
+            {text: "scripting", correct: false},
+            {text: "javascript", correct: true},
+            {text: "js", correct: true},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            {text: "script", correct: true},
+            {text: "scripting", correct: false},
+            {text: "javascript", correct: true},
+            {text: "js", correct: true},
+        ]
+    },
+];
+
+const question
