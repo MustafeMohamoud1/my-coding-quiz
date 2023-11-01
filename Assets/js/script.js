6c9e192d1058aad2e4a948af78ae4e7d22adb732
mustafemohamoud <mustyymo@outlook.com>
2023-11-01 23:25:18 +0000

    styling modified

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -124,7 +124,7 @@ while (playerName === null || playerName.trim() === "") {
       score++;
     } else {
       selectedBtn.classList.add("incorrect");
-      timeLeft -= 10;
+      timeLeft -= 20;
     }
     Array.from(answerButtons.children).forEach((button) => {
       if (button.dataset.correct === "true") {
